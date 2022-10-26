a3137831caa84c28d6ddf0bc396f5a5a974c9f73
D U Y Đ Ă N G <dangtd5401@gmail.com>
2022-10-26 17:42:49 +0700

    create quiz

--- app/src/main/java/com/coloful/activity/CreateStudySetActivity.java
+++ app/src/main/java/com/coloful/activity/CreateStudySetActivity.java
@@ -1,5 +1,7 @@
 package com.coloful.activity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -9,16 +11,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.coloful.R;
 import com.coloful.adapters.ListViewCreateSetAdapter;
-import com.coloful.dao.DBHelper;
 import com.coloful.dao.QuizDao;
 import com.coloful.datalocal.DataLocalManager;
 import com.coloful.model.Account;
+import com.coloful.model.Answer;
 import com.coloful.model.Question;
 
 import java.util.ArrayList;
@@ -28,39 +32,70 @@ public class CreateStudySetActivity extends AppCompatActivity implements View.On
     Button btnSave;
     ImageButton imgAdd;
     List<Question> questionList = new ArrayList<>();
-    EditText edt_create_quiz_title;
+    List<Answer> Answer = new ArrayList<>();
+    EditText edt_create_quiz_title, edt_definition_1, edt_term_1;
     Account account;
-
     ListView listView;
+    LinearLayout list;
+    Context context;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = this;
         setContentView(R.layout.activity_create_study_set);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#4257b0")));
 
         imgAdd = findViewById(R.id.img_add_question);
+        edt_create_quiz_title = findViewById(R.id.edt_create_quiz_title);
+        edt_term_1 = findViewById(R.id.edt_term_1);
+        edt_definition_1 = findViewById(R.id.edt_definition_1);
+        btnSave = findViewById(R.id.btn_save_set);
+
 
         questionList.add(new Question());
-        listView = findViewById(R.id.lv_create_set);
+//        listView = findViewById(R.id.lv_create_set);
         ListViewCreateSetAdapter adapter = new ListViewCreateSetAdapter(this, questionList);
-        listView.setAdapter(adapter);
+//        listView.setAdapter(adapter);
 
 //        btnSave.setOnClickListener(this::onClick);
         imgAdd.setOnClickListener(this::onClick);
         account = DataLocalManager.getAccount();
+        imgAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addView();
+            }
+        });
 
-        edt_create_quiz_title = findViewById(R.id.edt_create_quiz_title);
-        btnSave = findViewById(R.id.btn_save_set);
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 QuizDao db = new QuizDao();
-                db.addQuiz(CreateStudySetActivity.this,account.getUsername(),edt_create_quiz_title.getText().toString().trim());
+                db.addQuiz(CreateStudySetActivity.this,account.getUsername(),edt_create_quiz_title.getText().toString().trim(), questionList);
             }
         });
 
     }
+
+    private void addView(){
+        final View aa = getLayoutInflater().inflate(R.layout.list_view_create_set_item, null, false);
+        ImageView cross =(ImageView) aa.findViewById(R.id.cross2);
+        cross.setVisibility(View.VISIBLE);
+        cross.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeView(aa);
+            }
+        });
+        list.addView(aa);
+    }
+
+    private void removeView(View view){
+        list.removeView(view);
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- app/src/main/java/com/coloful/dao/QuizDao.java
+++ app/src/main/java/com/coloful/dao/QuizDao.java
@@ -4,6 +4,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
 
 import com.coloful.constant.Constant;
 import com.coloful.model.Account;
@@ -53,16 +54,36 @@ public class QuizDao {
 
         return null;
     }
-    public void addQuiz(Context context, String author, String title){
+
+    public void addQuiz(Context context, String author, String title, List<Question> questionList){
         db = new DBHelper(context);
         sqLiteDatabase = db.getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put(Constant.Quiz.TITLE.getValue(), title);
         cv.put(Constant.Quiz.AUTHOR.getValue(), author);
-        long result = sqLiteDatabase.insert(Constant.Quiz.TABLE_NAME.getValue(), null,cv);
-        if(result == -1){
+        long quizId = sqLiteDatabase.insert(Constant.Quiz.TABLE_NAME.getValue(), null,cv);
+        if(quizId == -1){
             Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
         }   else {
+            for (Question question : questionList) {
+                ContentValues values = new ContentValues();
+                values.put(Constant.Question.Content.getValue(), question.getContent());
+                values.put(Constant.Question.QUIZ_ID.getValue(), quizId);
+                long questionId = sqLiteDatabase.insert(Constant.Question.TABLE_NAME.getValue(), null, values);
+                if (questionId > 0) {
+                    ContentValues values1 = new ContentValues();
+                    values1.put(Constant.Answer.CONTENT.getValue(), question.getAnswer());
+                    values1.put(Constant.Answer.QUES_ID.getValue(), questionId);
+                    long answerId = sqLiteDatabase.insert(Constant.Answer.TABLE_NAME.getValue(), null, values1);
+
+                    if (answerId <= 0) {
+                        Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+            sqLiteDatabase.close();
             Toast.makeText(context, "Added success", Toast.LENGTH_SHORT).show();
         }
     }
