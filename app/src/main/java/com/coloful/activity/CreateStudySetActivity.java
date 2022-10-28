a49dfa02cb24ef28d7b70c4c890774b2d95f5640
luong.nguyen <Hienluong1405>
2022-10-28 13:37:51 +0700

    fix FE create set

--- app/src/main/java/com/coloful/activity/CreateStudySetActivity.java
+++ app/src/main/java/com/coloful/activity/CreateStudySetActivity.java
@@ -13,7 +13,9 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -33,14 +35,14 @@ public class CreateStudySetActivity extends AppCompatActivity {
     EditText edt_create_quiz_title;
     Account account;
     AutoCompleteTextView textIn, textIn1;
-    Button buttonAdd;
+    ImageButton buttonAdd;
     LinearLayout container;
     ArrayAdapter<String> adapter1;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_study_set);
         setContentView(R.layout.activity_create_study_set);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#4257b0")));
@@ -48,46 +50,51 @@ public class CreateStudySetActivity extends AppCompatActivity {
         edt_create_quiz_title = findViewById(R.id.edt_create_quiz_title);
         btnSave = findViewById(R.id.btn_save_set);
         questionList.add(new Question());
-        ListViewCreateSetAdapter adapter = new ListViewCreateSetAdapter(this, questionList);
         account = DataLocalManager.getAccount();
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 QuizDao db = new QuizDao();
-                db.addQuiz(CreateStudySetActivity.this,account.getUsername(),edt_create_quiz_title.getText().toString().trim(), questionList);
+                db.addQuiz(CreateStudySetActivity.this, account.getUsername(), edt_create_quiz_title.getText().toString().trim(), questionList);
             }
         });
         adapter1 = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line);
-        textIn = (AutoCompleteTextView)findViewById(R.id.textin);
-        textIn1 = (AutoCompleteTextView)findViewById(R.id.textin1);
-        textIn.setAdapter(adapter1);
-        textIn1.setAdapter(adapter1);
-        buttonAdd = (Button)findViewById(R.id.add);
+        buttonAdd = (ImageButton) findViewById(R.id.add);
         container = (LinearLayout) findViewById(R.id.container);
+        addItem();
+
         buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                LayoutInflater layoutInflater =
-                        (LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                final View addView = layoutInflater.inflate(R.layout.list_view_create_set_item, null);
-                AutoCompleteTextView textOut = (AutoCompleteTextView)addView.findViewById(R.id.edt_term_1);
-                AutoCompleteTextView textOut1 = (AutoCompleteTextView)addView.findViewById(R.id.edt_definition_1);
-                textOut.setAdapter(adapter1);
-                textOut1.setAdapter(adapter1);
-                textOut.setText(textIn.getText().toString());
-                textOut1.setText(textIn.getText().toString());
-                Button buttonRemove = (Button)addView.findViewById(R.id.remove);
-                final View.OnClickListener thisListener = new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v) {
-                        ((LinearLayout)addView.getParent()).removeView(addView);
-                    }
-                };
-                buttonRemove.setOnClickListener(thisListener);
-                container.addView(addView);
+                addItem();
             }
         });
+    }
+
+    public void addItem() {
+        LayoutInflater layoutInflater =
+                (LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View addView = layoutInflater.inflate(R.layout.list_view_create_set_item, null);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.setMargins(0, 15, 0, 15);
+        addView.setLayoutParams(params);
+        AutoCompleteTextView textOut = (AutoCompleteTextView) addView.findViewById(R.id.edt_term_1);
+        AutoCompleteTextView textOut1 = (AutoCompleteTextView) addView.findViewById(R.id.edt_definition_1);
+        textOut.setAdapter(adapter1);
+        textOut1.setAdapter(adapter1);
+        container.addView(addView);
+        Button buttonRemove = (Button) addView.findViewById(R.id.remove);
+        final View.OnClickListener thisListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((LinearLayout) addView.getParent()).removeView(addView);
+            }
+        };
+        buttonRemove.setOnClickListener(thisListener);
     }
 
     @Override
